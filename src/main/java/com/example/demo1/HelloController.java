def7cc43a22f2549d81f8670d88b85d7b76ab533
Assemgul <assemgul9@mail.ru>
2022-01-01 21:22:22 +0600

    lesson4

--- src/main/java/com/example/demo1/HelloController.java
+++ src/main/java/com/example/demo1/HelloController.java
@@ -1,14 +1,22 @@
 package com.example.demo1;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+  @FXML
+    TextArea chatWindow;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+  @FXML
+    TextField rowText;
+
+  public void sendText(ActionEvent actionEvent){
+
+      chatWindow.appendText(rowText.getText());
+      rowText.clear();
+      rowText.requestFocus();
+  }
 }
